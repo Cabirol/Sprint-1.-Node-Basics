78e72c1faad6d7b4afb9f4a011f2a60c0ec4e16d
Daniel L Acevedo <87641934+Cabirol@users.noreply.github.com>
2021-07-22 16:50:51 +0200

    Create entrega1.js

---
+++ entrega1.js
@@ -0,0 +1,15 @@
+//Nivell 1, exerici 1 ********************************
+
+function ImprimirNom(nom){
+	console.log(`El meu nom és ${nom}.`);
+}
+
+let nomUsuari = 'Daniel';
+ImprimirNom(nomUsuari);
+
+//Nivell 2, exercici 1 *******************************
+
+let cognom1 = "López",
+    cognom2 = "Acevedo";
+
+console.log(`El meu nom és ${nomUsuari} ${cognom1} ${cognom2}.`);
