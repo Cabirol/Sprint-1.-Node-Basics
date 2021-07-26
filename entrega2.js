0aef57cc841c48997ce2bb9dfcc091404a0ac585
Daniel L Acevedo <87641934+Cabirol@users.noreply.github.com>
2021-07-27 01:01:54 +0200

    Update entrega2.js

--- entrega2.js
+++ entrega2.js
@@ -31,3 +31,61 @@ personaExemple.dirNom();
 
 //Nivell 3, exercici 1 *************************************************
 
+class MtgCard{
+	constructor(nom, cmc, color){
+		this.nom = nom;
+		this.cmc = cmc;
+		this.color = color;
+		if(this.constructor == MtgCard){
+			throw new Error(" Object of Abstract Class cannot be created");
+			}	
+	}
+
+	info(){
+		throw new Error("Abstract Method has no implementation");
+	}
+}
+
+class Artifact extends MtgCard{
+
+	info(){
+		console.log(`${this.nom} is a ${this.color} artifact with cmc of ${this.cmc}.`);
+	}
+}
+
+class Creature extends MtgCard{
+	constructor(nom, cmc, color, power, toughness){
+		super(nom, cmc, color);
+		this.power = power;
+		this.toughness = toughness;
+	}
+
+	info(){
+		console.log(`${this.nom} is a ${this.power}/${this.toughness} ${this.color} creature with cmc of ${this.cmc}.`);
+	}
+}
+
+class Land extends MtgCard{
+	constructor(nom, cmc, color){
+		super(nom, cmc, color);
+		this.cmc = 0;
+		this.color = "colorless";
+	}
+
+	info(){
+		console.log(`${this.nom} is a land.`)
+	}
+}
+
+//Exemples:
+
+//var carta_0 = new MtgCard();    <--Dona error al ser implementat.
+
+var carta_1 = new Artifact("Black Lotus", 0, "colorless");
+carta_1.info();
+
+var carta_2 = new Creature("Snapcaster Mage", 2, "blue", 2, 1);
+carta_2.info();
+
+var carta_3 = new Land("Plains");
+carta_3.info();
