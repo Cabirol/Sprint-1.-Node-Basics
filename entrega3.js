dceaabaebdcd7dcef83087e0376259c696dd61da
Daniel L Acevedo <87641934+Cabirol@users.noreply.github.com>
2021-07-27 11:48:07 +0200

    Create entrega3.js

---
+++ entrega3.js
@@ -0,0 +1,21 @@
+//Nivell 1, exercici 1 ***********************************************************************
+
+let promesa_1 = new Promise(function(resolve, reject) {
+	
+	setTimeout(() => {				//simulem operació assíncrona amb un timeout
+		let correcte = true;		//simulem que la operació ha anat bé amb true (o malament amb false)
+      	if (correcte) {
+        	resolve('Tot ha anat bé');
+      	} else {
+        	reject(new Error("Hi ha hagut un error"));
+      	}
+	}, 1000);
+});
+
+promesa_1.then(
+  result => alert(result), 
+  error => alert(error) 
+);
+
+//Nivell 1, exercici 2 ************************************************************************
+
