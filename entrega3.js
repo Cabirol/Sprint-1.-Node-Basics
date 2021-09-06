d2df4ec1f0a5b77f84442d5146c7ffa3039b9163
Daniel L Acevedo <87641934+Cabirol@users.noreply.github.com>
2021-09-06 12:47:50 +0200

    Update entrega3.js

--- entrega3.js
+++ entrega3.js
@@ -1,34 +1,35 @@
 //Nivell 1, exercici 1 ***********************************************************************
 
-let promesa_1 = new Promise(function(resolve, reject) {
-	
-	setTimeout(() => {				//simulem operació assíncrona amb un timeout
-		let correcte = true;			//simulem que la operació ha anat bé amb true (o malament amb false)
-      		if (correcte) {
+var funció_1 = paràmetre_1 => {
+    let promesa = new Promise(function(resolve, reject) {
+      		if (paràmetre_1 == true) {
         		resolve('Tot ha anat bé');
       		} else {
         		reject(new Error("Hi ha hagut un error"));
       		}
-	}, 1000);
-});
+    });
 
-promesa_1.then(
-  result => console.log(result), 
-  error => console.log(error) 
-);
+    return promesa.then(
+        result => console.log(result), 
+        error => console.log(error) 
+    );
+}
+
+funció_1(true);
+funció_1(false);
 
 //Nivell 1, exercici 2 ************************************************************************
 
-var funció_1 = (paràmetre, callback) => {
-	if (paràmetre == true){
+var funció_2 = (paràmetre_2, callback) => {
+	if (paràmetre_2 == true){
 		callback("És veritat");
 	} else {
 		callback("És mentida");
 	}
 }
 
-funció_1(true, console.log);
-funció_1(false , console.log);
+funció_2(true, console.log);
+funció_2(false , console.log);
 
 //Nivell 2, exercici 1 *****************************************************************************************************
 
