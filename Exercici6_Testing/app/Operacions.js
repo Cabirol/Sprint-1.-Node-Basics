13e3e689d15f8fcfd63bfa3194efbd9cec2ed16c
Daniel L Acevedo <87641934+Cabirol@users.noreply.github.com>
2021-09-09 12:12:48 +0200

    Add files via upload

---
+++ Exercici6_Testing/app/Operacions.js
@@ -0,0 +1,24 @@
+function sumar(){
+    let suma = 0;
+    for (let i=0; i<arguments.length; i++){
+        suma += arguments[i];
+    }
+    return suma;
+}
+
+
+function restar(minuend, substraend){
+    return minuend - substraend;
+}
+
+function multiplicar(){
+    let mult = 1;
+    for (let i=0; i<arguments.length; i++){
+        mult *= arguments[i];
+    }
+    return mult;
+}
+
+function dividir(dividend, divisor){
+    return dividend/divisor;
+}
