a223baaeb2997d41bccdab46ed90c655674bb134
Daniel Acevedo <dlacevedo@protonmail.com>
2021-10-23 17:32:19 +0200

    petits detalls a la entrega 3

--- entrega3_Promises_Callbacks.js
+++ entrega3_Promises_Callbacks.js
@@ -12,11 +12,11 @@ var funcio_1 = parametre_1 => {
 
 funcio_1(true)
     .then(res => console.log(res))
-    .catch(err => console.log(err.message))
+    .catch(err => console.log(err.message));
 
 funcio_1(false)
 	.then(res => console.log(res))
-	.catch(err => console.log(err.message))
+	.catch(err => console.log(err.message));
 
 //Nivell 1, exercici 2 ************************************************************************
 
@@ -55,30 +55,21 @@ let salaries = [{
     salary: 2000
 }];
 
-
-let getEmployee = id_employee => (					
-	new Promise ( function(resolve, reject) {			
-  		for (var i=0; i<employees.length; i++) {
-            if (employees[i].id == id_employee) resolve (employees[i]);
-        }
-        reject(new Error("No s'ha trobat cap empleat amb aquest id."));
-	})
-);
-
-//getEmployee(1).then( result => console.log(result) );
+let getEmployee = id_employee => new Promise (function(resolve, reject) {			
+	for (var i=0; i<employees.length; i++) {
+		if (employees[i].id == id_employee) resolve (employees[i]);
+	}
+	reject(new Error("No s'ha trobat cap empleat amb aquest id."));
+});
 
 //Nivell 2, exercici 2 **********************************************************************************************
 
-let getSalary = employee_object => (					
-	new Promise ( function(resolve, reject) {									
-  		for (var i=0; i<salaries.length; i++) {
-			if (salaries[i].id == employee_object.id) resolve (salaries[i].salary);
-			}
-        reject(new Error("No s'ha trobat aquest objecte d'Empleat"));
-	})
-);
-
-//getSalary(employees[0]).then( result => console.log(result) );
+let getSalary = employee_object => new Promise (function(resolve, reject) {									
+	for (var i=0; i<salaries.length; i++) {
+		if (salaries[i].id == employee_object.id) resolve (salaries[i].salary);
+	}
+	reject(new Error("No s'ha trobat aquest objecte d'Empleat"));
+});
 
 //Nivell 2, exercici 3 ************************************************************************************************
 
@@ -88,18 +79,7 @@ getEmployee(1)
 
 //Nivell 3, exercici 1 ************************************************************************************************
 
-let getEmployeeMillorat = id_employee => (					//Modifiquem la funció getEmployee i l'anomenem getEmployeeMillorat...
-	new Promise ( function(resolve, reject) {			
-  		setTimeout( (arr, value) => {					//...no només perquè la lectura sigui menys farragosa (que també)...
-  			for (var i=0; i<arr.length; i++) {
-					if (arr[i].id == value) resolve (arr[i]);
-			}
-			reject (new Error ("ids només poden ser entre 1 i 3"));	//...sino per donar sentit a reject (saltarà error si posem un id fora de rang).
-		} , 1000, employees, id_employee);
-	})
-);
-
-getEmployeeMillorat(4)								//Ara hauria de sortir un error, ja que no hem definit id = 4.
+getEmployee(4)
 	.then(resolve => getSalary(resolve))
 	.then(resolve => console.log(resolve))
 	.catch(error => console.log(error));
